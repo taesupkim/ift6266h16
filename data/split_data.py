c00f5434b22b4d987e1b0e7d7a21b091de7de4e0
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-03-29 05:22:58 -0400

    split_data

--- data/split_data.py
+++ data/split_data.py
@@ -9,7 +9,7 @@ directory  = '/data/lisatmp4/taesup/data/YouTubeAudio/'
 youtube_id = 'XqaJ2Ol5cC4'
 
 wav_file = directory+youtube_id+'.wav'
-output_file = directory+youtube_id+'_train.hdf5'
+output_file = directory+youtube_id+'_valid.hdf5'
 
 _, data = scipy.io.wavfile.read(wav_file)
 if data.ndim == 1:
@@ -23,7 +23,7 @@ train_data = data[:,0:num_trains, :]
 valid_data = data[:,num_trains:, :]
 
 with h5py.File(output_file, 'w') as h5file:
-    fill_data = (('train', 'features', train_data),)
+    fill_data = (('train', 'features', valid_data),)
     print 'train_data : ', train_data.shape
     print 'valid_data : ', valid_data.shape
     fill_hdf5_file(h5file, fill_data)

13103fc216299749d241614064a56bfca970bb65
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-03-31 13:29:00 -0400

    gate feedback RNN

--- script/lstm_stack_model0.py
+++ script/lstm_stack_model0.py
@@ -402,8 +402,8 @@ def train_model(feature_size,
                 save_wavfile(sample_data, model_name+'_sample')
 
 if __name__=="__main__":
-    feature_size  = 16
-    hidden_size   = 64
+    feature_size  = 160
+    hidden_size   = 240
     learning_rate = 1e-4
     num_layers    = 3
     init_window   = 100

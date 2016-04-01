4034f08e72f81d8b720aaad1ef238b3ba9e44207
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-01 19:54:53 -0400

    gate feedback RNN

--- script/gf_rnn_model_0.py
+++ script/gf_rnn_model_0.py
@@ -122,12 +122,12 @@ def set_generator_update_function(generator_rnn_model,
                                         is_training=True)
 
     # get generator cost (time_length x num_samples x hidden_size)
-    generator_cost  = 0.5*tensor.inv(2.0*tensor.sqr(output_std_data))*tensor.sqr(output_mean_data-target_data)
-    generator_cost += tensor.log(output_std_data) + 0.5*tensor.log(2.0*numpy.pi)
+    generator_cost  = 0.5*tensor.inv(2.0*tensor.sqr(1.0))*tensor.sqr(output_mean_data-target_data)
+    generator_cost += tensor.log(1.0) + 0.5*tensor.log(2.0*numpy.pi)
 
     # set generator update
     generator_updates_cost = tensor.sum(generator_cost, axis=(0,2)).mean()
-    generator_updates_dict = get_model_updates(layers=generator_rnn_model+generator_mean_model+generator_std_model,
+    generator_updates_dict = get_model_updates(layers=generator_rnn_model+generator_mean_model,#+generator_std_model,
                                                cost=generator_updates_cost,
                                                optimizer=generator_optimizer,
                                                use_grad_clip=grad_clipping)
@@ -181,8 +181,10 @@ def set_generator_evaluation_function(generator_rnn_model,
                                         is_training=True)
 
     # get generator cost (time_length x num_samples x hidden_size)
-    generator_cost  = 0.5*tensor.inv(2.0*tensor.sqr(output_std_data))*tensor.sqr(output_mean_data-target_data)
-    generator_cost += tensor.log(output_std_data) + 0.5*tensor.log(2.0*numpy.pi)
+    # generator_cost  = 0.5*tensor.inv(2.0*tensor.sqr(output_std_data))*tensor.sqr(output_mean_data-target_data)
+    # generator_cost += tensor.log(output_std_data) + 0.5*tensor.log(2.0*numpy.pi)
+    generator_cost  = 0.5*tensor.inv(2.0*tensor.sqr(1.0))*tensor.sqr(output_mean_data-target_data)
+    generator_cost += tensor.log(1.0) + 0.5*tensor.log(2.0*numpy.pi)
 
     # set generator evaluate inputs
     generator_evaluate_inputs  = [source_data,

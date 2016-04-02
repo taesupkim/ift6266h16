7c2614543036e4bb38c05d1b15033ae3819695e7
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-02 03:57:06 -0400

    gate feedback RNN

--- script/gf_rnn_model_0.py
+++ script/gf_rnn_model_0.py
@@ -435,7 +435,7 @@ def train_model(feature_size,
 if __name__=="__main__":
     feature_size  = 16
     hidden_size   = 48
-    learning_rate = 1e-5
+    learning_rate = 1e-3
     num_layers    = 2
 
     model_name = 'gf_rnn_normal' \

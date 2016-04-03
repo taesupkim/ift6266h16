8558ac343a4d1eb233f61c135b1e8321b3a90aeb
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-03 01:01:12 -0400

    gate feedback RNN

--- script/lstm_gan_model_1.py
+++ script/lstm_gan_model_1.py
@@ -526,8 +526,8 @@ if __name__=="__main__":
 
         # set optimizer
         tf_generator_optimizer      = RmsProp(learning_rate=lr).update_params
-        gan_generator_optimizer     = RmsProp(learning_rate=lr).update_params
-        gan_discriminator_optimizer = RmsProp(learning_rate=lr).update_params
+        gan_generator_optimizer     = RmsProp(learning_rate=lr*0.1).update_params
+        gan_discriminator_optimizer = RmsProp(learning_rate=lr*0.1).update_params
 
 
         train_model(feature_size=feature_size,

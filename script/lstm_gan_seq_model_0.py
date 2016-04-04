144b01611c854720c312076b9743b63555302c4b
Taesup (TS) Kim <taesup.kim@umontreal.ca>
2016-04-04 03:05:54 -0400

    gate feedback RNN

--- script/lstm_gan_seq_model_0.py
+++ script/lstm_gan_seq_model_0.py
@@ -485,7 +485,7 @@ def train_model(feature_size,
 
 if __name__=="__main__":
     feature_size  = 1600
-    hidden_size   = 1000
+    hidden_size   =  800
     lr=1e-4
 
     model_name = 'LSTM_ONLY_GAN' \
@@ -504,7 +504,7 @@ if __name__=="__main__":
 
     # set optimizer
     tf_generator_optimizer      = RmsProp(learning_rate=0.0001).update_params
-    gan_generator_optimizer     = RmsProp(learning_rate=0.01).update_params
+    gan_generator_optimizer     = RmsProp(learning_rate=0.001).update_params
     gan_discriminator_optimizer = RmsProp(learning_rate=0.001).update_params
 
 
